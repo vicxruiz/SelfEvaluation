a2700978167e5fc91fecc94c54344bad5cd14e8e
Victor Ruiz <ruizdvictor@gmail.com>
2020-03-16 03:22:42 -0400

    added extension for email validation

--- SelfEvaluation/Extensions/String+Validation.swift
+++ SelfEvaluation/Extensions/String+Validation.swift
@@ -9,5 +9,12 @@
 import Foundation
 
 extension String {
-
+    func isValidEmail() -> Bool {
+        do {
+            let regex = try NSRegularExpression(pattern: "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,6}", options: .caseInsensitive)
+               return regex.firstMatch(in: self, options: NSRegularExpression.MatchingOptions(rawValue: 0), range: NSMakeRange(0, self.count)) != nil
+        } catch {
+            return false
+        }
+    }
 }
